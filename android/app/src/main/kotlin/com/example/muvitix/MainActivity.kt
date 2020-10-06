a5bfbe8e155c0f4adf4993328d1c5cfc5590b23e
afdlolnur <dddlllooolll@gmail.com>
2020-10-06 20:22:19 +0700

    auth firebase

---
+++ android/app/src/main/kotlin/com/example/muvitix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.muvitix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,4 @@
+part of 'shared.dart';
+
+String apiKey = "3247d3372cb8aafaf7b2bde22c751429";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
